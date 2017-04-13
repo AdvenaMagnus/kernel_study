959a2c9ad55d4ec583d958eb1f108b916202f4ec
AdvenaMagnus <alexfordeal@gmail.com>
2017-04-13 14:39:02 -0400

    add c

--- boot.asm
+++ boot.asm
@@ -1,8 +1,10 @@
 global start
+extern kernel_main
 
 section .text
 	bits 32
 	start:
-	mov dword [0xb8000], 0x2f4b2f4f
+	;mov dword [0xb8000], 0x2f4b2f4f
+	call kernel_main;
 	hlt
 
--- make.py
+++ make.py
@@ -1,12 +1,15 @@
 import os
 import shutil
 
+os.system("rm -r output")
+
 if not os.path.exists("output"):
 	os.makedirs("output")
 
 os.system("nasm -f elf multiboot_header.asm -o output/multiboot_header.o")
 os.system("nasm -f elf boot.asm -o output/boot.o")
-os.system("ld -n -o output/kernel.bin -T linker.ld output/multiboot_header.o output/boot.o")
+os.system("gcc -m32 -c kernel_main.c -o output/kernel_main.o")
+os.system("ld -n -o output/kernel.bin -T linker.ld output/multiboot_header.o output/boot.o output/kernel_main.o")
 
 isobootpath = "output/iso/boot"
 isogrubpath = isobootpath+"/grub"
