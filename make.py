cdf621fbc5998dc61f91af4c618af1fe9a2c1f90
AdvenaMagnus <alexfordeal@gmail.com>
2017-04-29 09:17:26 -0400

    Little modify

--- make.py
+++ make.py
@@ -8,7 +8,8 @@ if not os.path.exists("output"):
 
 os.system("nasm -f elf multiboot_header.asm -o output/multiboot_header.o")
 os.system("nasm -f elf boot.asm -o output/boot.o")
-os.system("gcc -m32 -c kernel_main.c -o output/kernel_main.o")
+#os.system("gcc -m32 -c kernel_main.c -o output/kernel_main.o")
+os.system("gcc -c kernel_main.c -o output/kernel_main.o")
 os.system("ld -n -o output/kernel.bin -T linker.ld output/multiboot_header.o output/boot.o output/kernel_main.o")
 
 isobootpath = "output/iso/boot"
